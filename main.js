cf791d6ff4e461c5ca8bfc6616753e1f91ec2719
j3ffh95 <j3ffh95@gmail.com>
2020-08-16 12:30:45 -0400

    Created updateCountdown function

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ const startingMinutes = 10;
 let time = startingMinutes * 60;
 
 const countdownEl = document.querySelector("#countdown");
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time * 60;
+
+  countdownEl.innerHTML = `${minutes}: ${seconds}`;
+  time--;
+}
